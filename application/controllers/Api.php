7bb66782078b11ccd3da706bae8f55cb9fdb4da0
Mukesh swamy <59552894+mukeshswamy@users.noreply.github.com>
2020-08-07 14:31:30 +0530

    CRUD ADDED

--- application/controllers/Api.php
+++ application/controllers/Api.php
@@ -9,6 +9,7 @@ class Api extends CI_Controller {
         parent::__construct();
         $this->load->model('ApiModel');
     }
+    // CREATE
     public function addProduct(){
         $this->form_validation->set_rules('productName', 'Product Name', 'required');
         $this->form_validation->set_rules('productPrice', 'Product Price', 'required');
@@ -30,9 +31,42 @@ class Api extends CI_Controller {
             $result = $this->ApiModel->saveProductAdded($data);
         }
     }
+    // READ
     public function showTotalProducts()
     {
         $result = $this->ApiModel->showProducts();
         echo json_encode($result);  
+    }
+    // UPDATE
+    public function updateProduct($id){
+        $this->form_validation->set_rules('productName', 'Product Name', 'required');
+        $this->form_validation->set_rules('productPrice', 'Product Price', 'required');
+        $this->form_validation->set_rules('productSKU', 'Product SKU', 'required');
+        $this->form_validation->set_rules('productQuantity', 'Product Quantity', 'required');
+        if($this->form_validation->run() == FALSE){
+            $response = array();
+            $response['status_code'] = 422;
+            $response['reason'] = "Validation Failed";
+            $response['validation_message'] = validation_errors();
+            $result = $this->output->set_status_header(422)->set_content_type('application/json')->set_output(json_encode($response));
+            return $result;
+        }else{
+            $data = array();
+            $data['product_name'] = $this->input->post('productName',TRUE);
+            $data['price'] = $this->input->post('productPrice',TRUE);
+            $data['sku'] = $this->input->post('productSKU',TRUE);
+            $data['quantity'] = $this->input->post('productQuantity',TRUE);
+            $result = $this->ApiModel->productUpdated($id,$data);
+        }
+    }
+    // GET_SINGLE_PRODUCT
+    public function showSingleProducts($id)
+    {
+        $result = $this->ApiModel->showSelectedProduct($id);
+        echo json_encode($result);  
+    }
+    // DELETE
+    public function deleteProduct($id){
+        $result = $this->ApiModel->deleteSelectedProduct($id);
     }
 }
--- application/models/ApiModel.php
+++ application/models/ApiModel.php
@@ -2,20 +2,52 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class ApiModel extends CI_Model {
+    // CREATE
     public function saveProductAdded($data){
-        $this->db->insert('products',$data);
+        $this->db->insert('users',$data);
         $userId = $this->db->insert_id();
         $response['status_code'] = 201;
         $response['response_message'] = "Product created with id: ".$userId;
         $res = $this->output->set_status_header(201)->set_content_type('application/json')->set_output(json_encode($response));
         return $res;
     }
+    // READ
     public function showProducts(){
         $this->db->select();
-        $query = $this->db->get('products');
+        $query = $this->db->get('users');
         if ($query->num_rows() > 0)
             return $query->result_array();
         else
             return $response= "No product Data Found";
+    }
+    // UPDATE
+     public function productUpdated($id,$data){
+        $this->db->select();
+        $this->db->where('ID', $id);
+        $this->db->update('users', $data);
+        $response['status_code'] = 201;
+        $response['response_message'] = "Product Updated with for id: ".$id;
+        $res = $this->output->set_status_header(201)->set_content_type('application/json')->set_output(json_encode($response));
+        return $res;
+    }
+    // GET_SINGLE_PRODUCT
+    public function showSelectedProduct($id){
+        $this->db->select();
+        $this->db->from('users');
+        $this->db->where('ID', $id);
+        $query = $this->db->get();
+        if ($query->num_rows() > 0)
+            return $query->result_array();
+        else
+            return $response= "No product Data Found";
+    }
+    // DELETE
+    public function deleteSelectedProduct($id){
+        $this->db->where('ID', $id);
+        $this->db->delete('users');
+        $response['status_code'] = 201;
+        $response['response_message'] = "Product Deleted for id: ".$id;
+        $res = $this->output->set_status_header(201)->set_content_type('application/json')->set_output(json_encode($response));
+        return $res;
     }
 }
